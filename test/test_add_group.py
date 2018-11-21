7df6f8d67ec7042b7163b718db09a97e5c77f2e4
Gena88 <32675403+Gena88@users.noreply.github.com>
2018-11-21 07:06:45 +0300

    Переход на стартовую страницу вынес в fixture

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,10 +2,8 @@
 from model.group import Group
 
 def test1_add_group(app):
-    app.open_houme_page()
     app.group.create(Group(name='n_test', header='h_test', footer='f_test'))
 
 
 def test2_add_empty_group(app):
-    app.open_houme_page()
     app.group.create(Group(name='test', header='header', footer='footer'))
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,5 +1,4 @@
 
 
 def test_delete_first_group(app):
-    app.open_houme_page()
-    app.group.delete_first_group()
+        app.group.delete_first_group()
